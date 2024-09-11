5944058313534f7e81d97d33984d4307c53b9317
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2024-09-11 10:32:41 -0600

    I made an equation!!!!!! :)))

---
+++ SRS/main.py
@@ -0,0 +1,17 @@
+#Locklin Sheldon, SkillPractice: Setting, Resetting, and Resseting.
+fs = int(32)
+s = int(100)
+g = int(2)
+t = int(12)
+
+print(fs+(s*g)/t)
+
+fs = int(29)
+fs = int(30)
+s = int(84)
+fifs = int(15)
+g = int(2)
+oneg = int(1)
+t = int(12)
+
+print(fs+((s*g)+(fifs*oneg))/t)
