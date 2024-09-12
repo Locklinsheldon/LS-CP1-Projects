04c660635a66ffe13c75419e836d6725c31bb7aa
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2024-09-12 10:32:06 -0600

    I made a whoareyou thingy

--- whoAreYou/main.py
+++ whoAreYou/main.py
@@ -1,10 +1,10 @@
 #Locklin Sheldon, SkillPractice: Who are you?
 #User's name, user's age, and favourite colour
 
-name = input("Enter your name! ")
+name = input("Enter your name!!! ")
 
-age = input("Enter your age! ")
+age = input("Enter your age!!! ")
 
-color = input("Enter your favourite color! ")
+color = input("Enter your favourite color!!! ")
 
 print("Your name is", name,", you are", age, "years old, and your favorite color is", color,)
