5728ddcf6bc69edeecf8a263be8d277ff56f74ec
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2024-09-24 10:39:56 -0600

    Palindrom calculator

---
+++ Palandrom.py
@@ -0,0 +1,9 @@
+pospal = input("Insert Word: ")
+revpal = pospal[::-1]
+print(revpal)
+str(pospal)
+str(revpal)
+if revpal == pospal:
+    print("This IS a palandrom :).")
+if revpal != pospal:
+    print("This is NOT a palandrom :(")
