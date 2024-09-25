5cce31639a0bf6e9c93f2391ae0d3c9bbd299f20
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2024-09-25 10:36:35 -0600

    another quizerbadizzythingalinga

---
+++ ProficiencyTest_Personal_Information_Converter.py
@@ -0,0 +1,8 @@
+name = input("Name here: ")
+age = input("Age here: ")
+height = input("Height here (meters): ")
+favnum = input("Favorite number here: ")
+a = int(age)
+f = int(favnum)
+h = float(height)
+print("Your name is:", name,"your age is", age, "your height is", height, "your favorite number is", favnum, "your new age is", a, "your new favorite number is",f, "and your new height is", h)
