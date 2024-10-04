ac31147b4050c4ae66ac1fdd33d59723fc682478
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2024-10-04 08:44:55 -0600

    ShoppyCarty

--- PTSLM.py
+++ PTSLM.py
@@ -1,6 +1,36 @@
-#Locklin Sheldon, ProficiencyTest: Shopping list manager.
-def y():
-    y = True
-def n():
-    n = True
-input("Add an item(0?")
+#Locklin Sheldon, SkillPractice: Shopping List Manager
+shoppingList = []
+
+
+def add():
+    item = input("Insert item: ")
+    shoppingList.append(item)
+    print("Item added.")
+    print("Current shopping list:", shoppingList)
+
+
+def remove():
+    item = input("Enter item to remove: ")
+    if item in shoppingList:
+        shoppingList.remove(item)
+        print("Item removed.")
+    else:
+        print("Item not found.")
+    print("Current shopping list:", shoppingList)
+
+
+while True:
+    action = input("""What would you like to do?
+                    Enter 1 to add item
+                    Enter 2 to remove item
+                    Enter 3 to leave the list:\n""")
+
+
+    if action == "1":
+        add()
+    elif action == "2":
+        remove()
+    else:
+        print("Have a nice day:)")
+       
+
---
+++ WATN.py
@@ -0,0 +1,3 @@
+formatnum = input("Insert: ")
+
+print("Here is your decimal de 4: ".format(num = formatnum))
