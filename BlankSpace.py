5eb6fc23d6729cdd0a3fab2c2b6a6240be79abe8
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2024-10-22 10:39:59 -0600

    I did tegh num pointer 4rtyr

--- BlankSpace.py
+++ BlankSpace.py
@@ -1,19 +1,8 @@
-# The function should return True if the number is between 10 and 20 (inclusive), but it doesn't. Find the bug.
+i = 1
 
-num = input("Number: ")
+int(i)
 
-int(num)
+while i <= 10:
+     i+1
 
-def is_between(num):
-
-     if num > 10 and num < 20:
-
-          return True
-
-     return False
-
-if True:
-     print("This number is between 10 and 20")
-
-     if False:
-          print("This number is not between 10 and 20")
+     print(i)
--- WATN.py
+++ WATN.py
@@ -1,3 +1,17 @@
-formatnum = input("Insert: ")
+num = int(input("Insert: "))
 
-print("Here is your decimal de 4: ".format(num = formatnum))
+fornum = "Commafied: {:,}"
+
+fourdec = "4 Decimal points: {:.4f}"
+
+pernum = int(num*100)
+
+dolnum = "Dollar decimal: {:.2f}"
+
+print(fornum.format(num))
+
+print(fourdec.format(num))
+
+print(pernum,"%")
+
+print(dolnum.format(num))
