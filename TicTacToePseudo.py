04592fa2a5c22d64edd611f698122bcd812e5c47
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2024-10-25 08:44:22 -0600

    fixedit

---
+++ TicTacToePseudo.py
@@ -0,0 +1,19 @@
+#Locklin Sheldon, Pseudocode Tic Tac Toe Program
+
+#Start game
+
+#"X" Goes first
+
+#"O" Goes second
+
+#Ask user if they would like to be "X" or "O"
+
+#If User picks "X", let them go first
+
+#Anything else is "O"
+
+#Loops back and forth between the user and computers turn
+
+#Display if the user won or lost the game
+
+#End game
---
+++ WIMG.py
@@ -0,0 +1,12 @@
+#Locklin Sheldon SkillPractice: What is my grade
+a = input("What is your Geography grade: ")
+b = input("CS 1400 A: ")
+c = input("Biology: ")
+d = input("Advisory: ")
+e = input("Digital Media: ")
+f = input("English: ")
+g = input("Math: ")
+list = str([a,b,c,d,e,f,g])
+print(list)
+
+if a > 
