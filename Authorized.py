8fc5cd1cf3ec7f13bd4b96a101f186feaf1002c9
Locklinsheldon <githubemail>
2024-11-06 10:30:39 -0700

    Made an authorixer

---
+++ Authorized.py
@@ -0,0 +1,23 @@
+User = ["Locklin", "Nathaniel", "Alexander", "Matthew", "Sigma", "Alpha", "Beta"]
+
+InList = input("What is your first name?: ")
+
+if InList == "Locklin":
+    print("You are on the list and an admin,", InList)
+elif InList == "LaRose":
+    print("You are on the list and an admin,", InList)
+elif InList == "Nathaniel":
+    print("You are in the list and a user,", InList)
+elif InList == "Alexander":
+    print("You are in the list and a user,", InList)
+elif InList == "Matthew":
+    print("You are in the list and a user,", InList)
+elif InList == "Sigma":
+    print("You are in the list and a user,", InList)
+elif InList == "Alpha":
+    print("You are in the list and a user,", InList)
+elif InList == "Beta":
+    print("You are in the list,", InList)
+else:
+    print("U r not on the list cuz ur not authorized :(")
+    
