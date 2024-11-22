8e90330712fe9482b8f0be6af1a59d2127b21514
Locklinsheldon <githubemail>
2024-11-22 10:36:39 -0700

    Counted up and THEN down

--- CountUpandDown.py
+++ CountUpandDown.py
@@ -1,1 +1,11 @@
-#Locklin Sheldon
+#Locklin Sheldon, SkillPractice: Count Up and Down
+
+twen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+revtwen = [19,18,17,16,15,14,13,12,11,19,9,8,7,6,5,4,3,2,1]
+
+for i in twen:
+    print(i)
+
+for i in revtwen:
+    print(i)
