96cdc152cc6d156ee45385ebddedc20a06795b4a
Locklinsheldon <githubemail>
2024-12-04 10:46:17 -0700

    Made teh quizergngngngngnnnnnnnnnn

---
+++ SimpleQuizGame.py
@@ -0,0 +1,174 @@
+
+def quesone():
+    while True: 
+        print("What is 1+1?")
+        print("A. 2")
+        print("B. fish")
+        print("C. 11")
+        print("D. None of the above")
+
+        choice11 = input("Enter your choice (A, B, C, D): ")
+
+        if choice11 == "A":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Easier question: What is 7/3.5?")
+        print("A. 4")
+        print("B. 7")
+        print("C. 2")
+        print("D. None of the above")
+
+        choice12 = input("Enter your choice (A, B, C, D): ")
+
+        if choice12 == "C":
+            print("Correct!")
+        else:
+            print("You got it wrong.")
+            break
+quesone()
+
+def questwo():
+    while True:
+        print("What is 5+5?")
+        print("A. 12")
+        print("B. 10")
+        print("C. 3")
+        print("D. None of the above")
+
+        choice3 = input("Enter your choice (A, B, C, D): ")
+
+        if choice3 == "B":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Easier question: What is 2+2?")
+        print("A. 6")
+        print("B. 7")
+        print("C. 1")
+        print("D. None of the above")
+
+        choice4 = input("Enter your choice (A, B, C, D): ")
+
+        if choice4 == "D":
+            print("Correct!")
+        else:
+            print("You got it wrong.")
+            break
+questwo()
+
+def questhree():
+    while True:
+        print("What is 5x5?")
+        print("A. 25")
+        print("B. 30")
+        print("C. 10")
+        print("D. None of the above")
+
+        choice5 = input("Enter your choice (A, B, C, D): ")
+
+        if choice5 == "A":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Easier question: What is 2x2?")
+        print("A. 9")
+        print("B. 4")
+        print("C. 2")
+        print("D. None of the above")
+
+        choice6 = input("Enter your choice (A, B, C, D): ")
+
+        if choice6 == "B":
+            print("Correct!")
+        else:
+            print("You got it wrong.")
+            break
+questhree()
+
+def quesfour():
+    while True:
+        print("What is 5/5?")
+        print("A. 7")
+        print("B. 10")
+        print("C. 1")
+        print("D. None of the above")
+
+        choice7 = input("Enter your choice (A, B, C, D): ")
+
+        if choice7 == "C":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Easier question: What is 2/1?")
+        print("A. 2")
+        print("B. 8")
+        print("C. 1")
+        print("D. None of the above")
+
+        choice8 = input("Enter your choice (A, B, C, D): ")
+
+        if choice8 == "A":
+            print("Correct!")
+        else:
+            print("You got it wrong.")
+            break
+quesfour()
+
+def quesfive():
+    while True:
+        print("What is 9-4?")
+        print("A. 5")
+        print("B. 4")
+        print("C. 9")
+        print("D. None of the above")
+
+        choice9 = input("Enter your choice (A, B, C, D): ")
+
+        if choice9 == "A":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Easier question: What is 9/0?")
+        print("A. 1")
+        print("B. 9")
+        print("C. 0/9")
+        print("D. None of the above")
+
+        choice10 = input("Enter your choice (A, B, C, D): ")
+
+        if choice10 == "D":
+            print("Correct!")
+        else:
+            print("You got it wrong.")
+            break
+quesfive()
+
+def quesbonus():
+    while True: 
+        print("Bonus question: What is Obama's last name?")
+        print("A. Obama")
+        print("B. Barack")
+        print("C. Michelle")
+        print("D. None of the above")
+
+        choice1 = input("Enter your choice (A, B, C, D): ")
+
+        if choice1 == "A":
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Easier question: What is Mr.s LaRose's last name?")
+        print("A. Lugubrious")
+        print("B. Mr.s")
+        print("C. LaRose")
+        print("D. None of the above")
+
+        choice2 = input("Enter your choice (A, B, C, D): ")
+
+        if choice2 == "C":
+            print("Correct!")
+        else:
+            print("You got it wrong.")
+            break
+quesbonus()
