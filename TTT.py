dfc705de73aad86e5dbd479ccaa72e071655c8ab
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2024-12-07 03:18:13 +0000

    I did it again

--- TTT.py
+++ TTT.py
@@ -12,14 +12,12 @@ def PrintBoard(board):
         print()
 
 def CheckWhoWon(board):
-    # Check rows and columns
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2] != " ":
             return board[i][0]
         if board[0][i] == board[1][i] == board[2][i] != " ":
             return board[0][i]
 
-    # Check diagonals
     if board[0][0] == board[1][1] == board[2][2] != " ":
         return board[0][0]
     if board[0][2] == board[1][1] == board[2][0] != " ":
